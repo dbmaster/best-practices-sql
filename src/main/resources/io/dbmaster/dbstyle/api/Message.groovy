4f9f8c98dc4397744b019b950e05d298c6a39a7c
Slava Christin <slava@branegy.com>
2015-08-06 01:28:10 -0400

    Remove semicolons

--- src/main/resources/io/dbmaster/dbstyle/api/Message.groovy
+++ src/main/resources/io/dbmaster/dbstyle/api/Message.groovy
@@ -1,17 +1,17 @@
 package io.dbmaster.dbstyle.api
 
 public final class Message {
+    
+    String object_type
 
-    String object_type;
+    String object_name
 
-    String object_name;
+    String severity
 
-    String severity;
-
-    String description;
+    String description
 
     /* Useful when copying list for further troubleshooting / automation */
-    String object_key;
+    String object_key
     
 
     public Message(Map properties=null) {
