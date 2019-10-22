1422774d0a787802dc00c7f49ebaa5aba5cbd4ac
vitalh <vitaly@branegy.com>
2019-10-22 16:22:48 +0200

    update test

--- src/test/groovy/BestPractiesSimpleIT.groovy
+++ src/test/groovy/BestPractiesSimpleIT.groovy
@@ -1,3 +1,5 @@
+import static org.testng.Assert.assertTrue
+
 import org.testng.annotations.Test
 
 import io.dbmaster.testng.BaseToolTestNGCase;
@@ -8,18 +10,34 @@ public class BestPractiesSimpleIT extends BaseToolTestNGCase {
     @Test
     public void test() {
         def config ="""
-                    <config>
-                        <checkSet scope=\"all\">
-                            <check name=\"FragmentedIndexes\">
-                                <property name=\"min_fragmentation\" value=\"40\" />
-                                <property name=\"min_page_count\"    value=\"501\" />
-                            </check>
-                            <check name=\"UserObjectSystemDB\" />
-                        </checkSet>
-                    </config>"""
+<config>
+    <scope name="non-system">
+       <databases include="System&lt;&gt;1" exclude="" />
+       <objects    include="type=Table &amp;&amp; category=Test"  />
+    </scope>
+    <scope name="dev_all"  >
+       <servers include="Environment=Development" />
+    </scope>
+
+    <checkSet scope="dev_all">
+        <check name="FragmentedIndexes">
+            <property name="min_fragmentation" value="40" />
+            <property name="min_page_count"    value="501" />
+        </check>
+        <check name="UserObjectSystemDB">
+            <property name="sysdb.valid_object"    value="msdb.dbo.sysdtslog90" />
+            <property name="sysdb.valid_object"    value="master.dbo.sp_WhoIsActive" />
+        </check>
+    </checkSet>
+    <checkSet scope="dev_all">
+
+    </checkSet>
+</config>
+                    """
 
         def parameters = [ "p_config"     :  config ]
         String found_tables = tools.toolExecutor("best-practices-sql", parameters).execute()
         System.out.println("done! "+found_tables)
+        assertTrue(found_tables.contains("Server"));
     }
 }
